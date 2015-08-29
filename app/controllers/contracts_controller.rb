69ca8fbb722f5b57bdfaa4d9c3fa8f36f437d542
Steven Abrams <steven.abrams86@gmail.com>
2015-08-29 17:26:02 -0600

    contracts working with simple auth

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,5 +1,5 @@
 class ContractsController < ApplicationController
-
+  before_filter :authenticate_user!
   def new
     @user = current_user
     @item = Item.find(params[:item_id])
@@ -8,10 +8,16 @@ class ContractsController < ApplicationController
 
   def show
     @contract = Contract.find(params[:id])
+    if (@contract.renter_id == current_user.id) || (@contract.owner_id == current_user.id)
+    else
+      redirect_to items_path
+    end
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @contract = Contract.new(contract_params)
+    @contract.owner_id = @item.user_id
     if @contract.save
       redirect_to user_item_contract_path(@contract.owner_id, @contract.item_id, @contract.id)
     else
@@ -22,7 +28,7 @@ class ContractsController < ApplicationController
   private
 
   def contract_params
-    params.require(:contract).permit(:start_date, :end_date, :owner_id, :renter_id, :item_id).merge(item_id: params[:item_id], owner_id: params[:user_id], renter_id: current_user.id)
+    params.require(:contract).permit(:start_date, :end_date, :owner_id, :renter_id, :item_id).merge(item_id: params[:item_id], renter_id: current_user.id)
   end
 
 end
